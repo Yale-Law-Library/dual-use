f79b29aa5f1da6e725020a0f96f9c32dd9c15162
Parker Kaufmann <pkaufmann112@gmail.com>
2025-06-02 10:45:52 -0400

    Updates

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Bennett Text';
+    src: url('./BennettTextFour.otf') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  body {
+    font-family: 'Bennett Text', Georgia, serif;
+    margin: 0;
+    padding: 0;
+    background: white;
+    color: #333;
+  }
+`;
